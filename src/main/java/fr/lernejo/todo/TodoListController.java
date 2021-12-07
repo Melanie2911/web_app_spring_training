d16f5cedc26465f2a5aeac03050629e26cadd343
Melanie2911 <m2e9l1a1n2i0e0@gmail.com>
2021-12-07 19:33:42 +0100

    PART7

--- src/main/java/fr/lernejo/todo/TodoListController.java
+++ src/main/java/fr/lernejo/todo/TodoListController.java
@@ -1,20 +1,28 @@
 package fr.lernejo.todo;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.ArrayList;
-import java.util.List;
+
 @RestController
-public class TodoListController {
-    private List<Todo> liste = new ArrayList<>();
-    @PostMapping("api/todo")
-    public void AddList(@RequestBody Todo newTodo){
-        liste.add(newTodo);
+public class TodoListController  {
+    TodoRepository repo;
+    public TodoListController(TodoRepository rep)
+    {
+        this.repo=rep;
     }
-    @GetMapping("api/todo")
-    public List<Todo> SendList() {
-        return liste;
+    @PostMapping(value = "/api/todo")
+    public TodoEntity ajout(@RequestBody TodoEntity todo)
+    {
+        return repo.save(todo);
+
+    }
+    @GetMapping(value = "/api/todo")
+    public Iterable<TodoEntity> affichage()
+    {
+        return repo.findAll();
+
     }
 }
 
